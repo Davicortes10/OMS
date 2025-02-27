bd73b0def690adf7efafde3db5aee8e2d8292c0b
DaviCortesDev <davicortes1@hotmail.com>
2025-02-27 10:52:29 -0300

    Classe Duplicata inicial adicionado

---
+++ preprocessamento/analise/duplicatas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Duplicatas:
+
+    def __init__(self, df: pd.DataFrame):
+        """
+        Inicializa a classe com um DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame que será analisado.
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("❌ O argumento fornecido deve ser um DataFrame do Pandas.")
+        
+        self.df = df
+
+    

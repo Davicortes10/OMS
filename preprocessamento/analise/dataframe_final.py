07594eb524e6a9d13a43946967dbe2e7650bccd7
DaviCortesDev <davicortes1@hotmail.com>
2025-02-27 12:00:12 -0300

    Função verificar_valores_ausentes_por_pais não documentada adicionada

--- preprocessamento/analise/dataframe_final.py
+++ preprocessamento/analise/dataframe_final.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
 
-class DataCleaner:
+class DataFrameFinal:
 
     def __init__(self, df: pd.DataFrame):
         """
@@ -15,3 +15,20 @@ class DataCleaner:
             raise TypeError("❌ O argumento fornecido deve ser um DataFrame do Pandas.")
 
         self.df = df.copy()  # Mantém os dados originais intactos
+    
+    def verificar_valores_ausentes_por_pais(self) -> pd.Series:
+        
+        if 'Country' not in self.df.columns:
+            raise KeyError("❌ O DataFrame deve conter a coluna 'Country' para segmentação dos dados.")
+
+        valores_faltantes = (
+            self.df.isnull()
+            .groupby(self.df['Country'])
+            .sum()
+            .sum(axis=1)
+            .sort_values(ascending=False)
+        )
+
+        print("\n📉 Contagem de valores ausentes por país:")
+        print(valores_faltantes)
+        return valores_faltantes

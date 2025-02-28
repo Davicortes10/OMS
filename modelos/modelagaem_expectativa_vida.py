77ce0deb2f0f880599fe739c3dc31ee379f651be
DaviCortesDev <davicortes1@hotmail.com>
2025-02-27 23:45:16 -0300

    Construindo o modelo de rede neural

---
+++ modelos/modelagaem_expectativa_vida.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+
+class LifeExpectancyNN:
+    def __init__(self, df: pd.DataFrame):
+        """
+        Inicializa a classe, realizando a preparação dos dados.
+
+        Args:
+            df (pd.DataFrame): O DataFrame com os dados originais.
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("❌ O argumento fornecido deve ser um DataFrame do Pandas.")
+
+        self.df = df.copy()
+        self.label_cols = ['Country', 'Status']
+        self.scale_cols = [col for col in df.columns if col not in self.label_cols + ['Life expectancy ']]
+        
+        self.pre_processed_csv = self.preprocess_data(df)
+        self.X = self.pre_processed_csv.drop('Life expectancy ', axis=1)
+        self.y = self.pre_processed_csv['Life expectancy ']
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.X, self.y, test_size=0.2, random_state=123
+        )
+
+        self.model = self.build_model(input_shape=(self.X_train.shape[1],))
